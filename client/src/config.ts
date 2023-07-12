1db0548dff0b6bbc8101bdba08a05e633f5e8172
AbhinavReddy-Dev <abhinavreddy9681@gmail.com>
2023-07-12 01:20:35 -0400

    updated nginx conf 12

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,3 +1,3 @@
 export const server_url = `http://${
-  import.meta.env.NODE_ENV === "production" ? "18.208.145.36" : "localhost"
+  import.meta.env.PROD ? "18.208.145.36" : "localhost"
 }:5050/api/v1/`;
